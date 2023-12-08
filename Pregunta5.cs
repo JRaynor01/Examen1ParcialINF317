81e7ecfc1a99ffeee58870e9c70a57b16d7e34fb
JRaynor01 <95384665+JRaynor01@users.noreply.github.com>
2023-12-07 21:44:54 -0400

    Update Pregunta5.cs

--- Pregunta5.cs
+++ Pregunta5.cs
@@ -1,71 +1,32 @@
 using System;
-using System.Collections.Generic;
-using System.Threading;
+using System.Threading.Tasks;
 
 class Program
 {
-    static List<int> serie = new List<int>();
-    static object lockObj = new object();
-
-    static void GenerarSerie(object posiciones)
-    {
-        int numPosiciones = (int)posiciones;
-
-        lock (lockObj)
-        {
-            serie.Add(2);
-        }
-
-        for (int i = 1; i < numPosiciones; i++)
-        {
-            int nuevoValor;
-
-            if (i % 2 == 1)
-            {
-                nuevoValor = i + 1;
-            }
-            else
-            {
-                lock (lockObj)
-                {
-                    nuevoValor = serie[i - 1] + serie[i - 2] + 1;
-                }
-            }
-
-            lock (lockObj)
-            {
-                serie.Add(nuevoValor);
-            }
-        }
-    }
-
     static void Main()
     {
         int numPosiciones = 10000;
-        int numHilos = 4; // Establece el número de hilos deseados
+        int[] serie = new int[numPosiciones];
+        serie[0] = 2;
 
-        List<Thread> hilos = new List<Thread>();
-
-        for (int i = 0; i < numHilos; i++)
+        Parallel.For(1, numPosiciones, i =>
         {
-            Thread hilo = new Thread(GenerarSerie);
-            hilos.Add(hilo);
-        }
-
-        foreach (Thread hilo in hilos)
-        {
-            hilo.Start(numPosiciones / numHilos);
-        }
-
-        foreach (Thread hilo in hilos)
-        {
-            hilo.Join();
-        }
+            if (i % 2 == 1)
+            {
+                serie[i] = i + 1;
+            }
+            else
+            {
+                serie[i] = serie[i - 1] + serie[i - 2] + 1;
+            }
+        });
 
         // Imprimir los primeros 10 elementos de la serie
         for (int i = 0; i < 10; i++)
         {
             Console.Write(serie[i] + " ");
         }
+
+        Console.WriteLine();
     }
 }
